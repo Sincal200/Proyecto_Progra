0bd5655e4e379b7cfd6222ed677d97970816a624
Jesús Ruiz <chuspop4@gmail.com>
2022-05-19 21:39:28 -0600

    Revert "prueba de imágenes"
    This reverts commit 8a1826a6233d72ff1398a213e5ff5e0e374bb0f5.

--- Proyecto_Progra/src/main/java/com/mycompany/proyecto_progra/Login.java
+++ Proyecto_Progra/src/main/java/com/mycompany/proyecto_progra/Login.java
@@ -6,13 +6,6 @@
 package com.mycompany.proyecto_progra;
 
 import javax.swing.JOptionPane;
-import java.awt.Image;
-
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 /**
  *
@@ -41,7 +34,6 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         txtUsername = new javax.swing.JTextField();
         jPasswordField1 = new javax.swing.JPasswordField();
-        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -61,13 +53,6 @@ public class Login extends javax.swing.JFrame {
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 130, -1, -1));
         getContentPane().add(txtUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 80, 171, -1));
         getContentPane().add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 120, 171, -1));
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/afdsaa.png"))); // NOI18N
-        jLabel3.setText("jLabel3");
-        jLabel3.setMaximumSize(new java.awt.Dimension(500, 500));
-        jLabel3.setMinimumSize(new java.awt.Dimension(500, 500));
-        jLabel3.setPreferredSize(new java.awt.Dimension(500, 500));
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 110, 100));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -135,7 +120,6 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JButton btnLogin;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
