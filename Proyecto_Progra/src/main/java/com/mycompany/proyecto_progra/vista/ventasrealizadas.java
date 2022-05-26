16e3b57b8f32c036373d9f289a8ff31700cd3571
ed225 <eduardoalvizures21@gmail.com>
2022-05-25 22:06:17 -0600

    boton guardar
    se realizo el boton de guardar

---
+++ Proyecto_Progra/src/main/java/com/mycompany/proyecto_progra/vista/ventasrealizadas.java
@@ -0,0 +1,65 @@
+
+package com.mycompany.proyecto_progra.vista;
+
+public class ventasrealizadas {
+    private String code;
+    private String product;
+    private String uom;
+    private String quantity;
+    private String price;
+    private String amount;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public ventasrealizadas() {
+    }
+
+    
+    
+}
